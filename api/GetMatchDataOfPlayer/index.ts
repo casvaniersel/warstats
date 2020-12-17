1cf0df3dab1c2da1203572aad885fb56939c5f2c
Stefan Bertens <stefanbertens@outlook.com>
2020-12-17 23:16:58 +0100

    Change: test with c4zler instead of kuubs

--- api/GetMatchDataOfPlayer/index.ts
+++ api/GetMatchDataOfPlayer/index.ts
@@ -13,7 +13,7 @@ enum Platform {
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     // Init API
-    const _api = API({ platform: Platform.xbl });
+    const _api = API({ platform: Platform.battle });
 
     // Getting credentials from KeyVault
     const activisionUserName = process.env["ActivisionUserName"];
@@ -24,6 +24,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     // Get vars
     const name = (req.query.name || (req.body && req.body.name));
     const platform: Platform = (req.query.platform || (req.body && req.body.platform) || Platform.xbl);
+    const from = (req.query.from || (req.body && req.body.from));
+    const to = (req.query.to || (req.body && req.body.to));
 
     try {
         //Auth
@@ -35,8 +37,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         let data: Welcome = await _api.MWcombatwz(name, platform);
 
         // Get KD
-        let from: number = await new Date(2020,12,17,21,0,0,0).getMilliseconds();
-        let to: number = await new Date(2020,12,17,23,0,0,0).getMilliseconds();
+        let from: number = new Date("2020-12-17T20:00:00+0100").getMilliseconds();
+        let to: number = new Date("2020-12-17T23:00:00+0100").getMilliseconds();
         let matchesbetweentimespan: Match[] = data.matches.filter(x => x.utcStartSeconds > from && x.utcEndSeconds < to)
 
         // Output
