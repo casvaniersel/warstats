3e4444dfff15656622f3562d32d1279b1ef606ab
Stefan Bertens <stefanbertens@outlook.com>
2020-12-17 23:26:18 +0100

    Add: from till option in query

--- api/GetMatchDataOfPlayer/index.ts
+++ api/GetMatchDataOfPlayer/index.ts
@@ -37,8 +37,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         let data: Welcome = await _api.MWcombatwz(name, platform);
 
         // Get KD
-        let from: number = new Date("2020-12-17T20:00:00+0100").getMilliseconds();
-        let to: number = new Date("2020-12-17T23:00:00+0100").getMilliseconds();
+        //let from: number = new Date("2020-12-17T20:00:00+0100").getMilliseconds();
+        //let to: number = new Date("2020-12-17T23:00:00+0100").getMilliseconds();
         let matchesbetweentimespan: Match[] = data.matches.filter(x => x.utcStartSeconds > from && x.utcEndSeconds < to)
 
         // Output
